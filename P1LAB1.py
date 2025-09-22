0e316158100235e0c7c19aae2ce6ea3c2e67f423
tegan Reed <morrist9724@student.faytechcc.edu>
2025-09-22 10:29:46 -0400

    Add P1LAB1.py

---
+++ P1LAB1.py
@@ -0,0 +1,7 @@
+#Tegan Reed
+#9/21/2025
+#P1LAB1
+#Trying out Python 
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
